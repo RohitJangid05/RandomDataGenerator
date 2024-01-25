85987a190a3f335878a7eeefb1bd614d0163dc98
RohitJangid05 <jangidrohit5820@gmail.com>
2024-01-25 19:15:51 +0530

    array file added

---
+++ array.js
@@ -0,0 +1,12 @@
+let Ename = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Henry", "Ivy", "Jack", "Katherine", "Liam", "Mia", "Noah", "Olivia", "Peter", "Quinn", "Rachel", "Samuel", "Taylor"];
+
+let Eposition = ["Software Engineer", "Data Scientist", "Web Developer", "Network Engineer", "System Administrator", "Database Administrator", "IT Project Manager", "UI/UX Designer", "Security Analyst", "DevOps Engineer", "QA Engineer", "Business Analyst", "IT Support Specialist", "Network Administrator", "Technical Writer", "IT Consultant", "Cloud Solutions Architect", "Machine Learning Engineer", "Cybersecurity Specialist", "IT Director",
+]
+
+let Ecity = ["Bengluru", "Pune", "Nodia", "Gurugoan", "Mumbai", "Chennai", "Hydrebad"];
+
+module.exports = {
+    Ename,
+    Eposition,
+    Ecity
+};
