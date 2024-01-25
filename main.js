b8edb0a84c0062d28229a8cdf37fddda6cc2381e
RohitJangid05 <jangidrohit5820@gmail.com>
2024-01-25 19:00:41 +0530

    Updated employee table

--- main.js
+++ main.js
@@ -22,14 +22,18 @@ app.get('/generate', async (req, res) => {
 
     await Employee.deleteMany({});
     let Ename = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Henry", "Ivy", "Jack", "Katherine", "Liam", "Mia", "Noah", "Olivia", "Peter", "Quinn", "Rachel", "Samuel", "Taylor"];
-    let Elan = ["Python", "C++", "C", "Java"];
+    
+    let Eposition = ["Software Engineer","Data Scientist","Web Developer","Network Engineer","System Administrator","Database Administrator","IT Project Manager","UI/UX Designer","Security Analyst","DevOps Engineer","QA Engineer","Business Analyst","IT Support Specialist","Network Administrator","Technical Writer","IT Consultant","Cloud Solutions Architect", "Machine Learning Engineer", "Cybersecurity Specialist", "IT Director",
+    ]
+
+    // let Elan = ["Python", "C++", "C", "Java"];
     let Ecity = ["Bengluru", "Pune", "Nodia", "Gurugoan", "Mumbai", "Chennai", "Hydrebad"];
 
     for (i = 0; i < 10; i++) {
         let e = await Employee.create({
             name: random(Ename),
             salary: Math.floor(Math.random() * 20000),
-            language: random(Elan),
+            position: random(Eposition),
             city: random(Ecity)
         })
     }
--- model/employee.js
+++ model/employee.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 const employeeSchema =new mongoose.Schema({
     name: String,
     salary:Number,
-    language:String,
+    position:String,
     city:String
 })
 
